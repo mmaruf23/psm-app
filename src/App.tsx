c7f47bb3aec883377dbdc28bea292771624c6737
mmaruf <maruuf82@gmail.com>
2025-11-29 22:51:05 +0700

    Initial Project
    Installed :
    - Vite
    - React
    - Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-black py-10 text-white">
+        <p className="text-6xl underline">Ini mah bukan apa-apa</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
